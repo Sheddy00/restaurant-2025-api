fcbda137e5e7482eada9b18a030ab3b0e8d3de26
Sheddy <hei.sheddy.2@gmail.com>
2025-04-19 17:55:16 +0300

    chore: add record class DishStatusUpdateRest

--- src/main/java/edu/hei/school/restaurant/endpoint/rest/DishStatusUpdateRest.java
+++ src/main/java/edu/hei/school/restaurant/endpoint/rest/DishStatusUpdateRest.java
@@ -2,5 +2,5 @@ package edu.hei.school.restaurant.endpoint.rest;
 
 import edu.hei.school.restaurant.model.enums.StatusEnum;
 
-public record DishStatusUpdateRequest(StatusEnum newStatus) {
+public record DishStatusUpdateRest(StatusEnum newStatus) {
 }
